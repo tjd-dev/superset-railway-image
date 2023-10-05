66069b65e8bc93f1e3180d8d5814b26cb6f9d738
Leonardo <leo@inflame.app>
2023-10-05 18:31:59 -0300

    Remove unnecessary comments

--- superset_config.py
+++ superset_config.py
@@ -4,8 +4,6 @@ logger = logging.getLogger()
 
 PYTHONPATH = $PYTHONPATH
 
-# database configurations (do not modify)
-
 RATELIMIT_STORAGE_URI = $SUPERSET_CACHE_REDIS_URL
 SQLALCHEMY_DATABASE_URI = $SQLALCHEMY_DATABASE_URI
 SUPERSET_CACHE_REDIS_URL = $SUPERSET_CACHE_REDIS_URL
@@ -21,7 +19,6 @@ CACHE_CONFIG = {
 }
 DATA_CACHE_CONFIG = CACHE_CONFIG
 
-#redis-cli -u redis://default:JcOpyrs7uZlse7cCzlDu@containers-us-west-198.railway.app:7283
 class CeleryConfig(object):
     BROKER_URL = f$SUPERSET_CACHE_REDIS_URL + "/1"
     CELERY_IMPORTS= ("superset.sql_lab",)
@@ -30,22 +27,10 @@ class CeleryConfig(object):
 
 CELERY_CONFIG = CeleryConfig
 
-# database engine specific ironment variables
-# change the below if you prefer another database engine
-#POSTGRES_DB = $POSTGRES_DB
-#POSTGRES_USER = $POSTGRES_USER
-#POSTGRES_PASSWORD = $POSTGRES_PASSWORD
-#MYSQL_DATABASE=superset
-#MYSQL_USER=superset
-#MYSQL_PASSWORD=superset
-#MYSQL_RANDOM_ROOT_PASSWORD=yes
-
-# Add the mapped in /app/pythonpath_docker which allows devs to override stuff
 REDIS_HOST = $REDIS_HOST
 REDIS_PORT = $REDIS_PORT
 
 SUPERSET_ENV = $SUPERSET_ENV
 SUPERSET_LOAD_EXAMPLES = $SUPERSET_LOAD_EXAMPLES
 SUPERSET_SECRET_KEY = $SUPERSET_SECRET_KEY
-# CYPRESS_CONFIG = $CYPRESS_CONFIG
 SUPERSET_PORT = $SUPERSET_PORT
